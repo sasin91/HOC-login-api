e226461de819d6a645a4df00aa3a7243bb6d5bd7
Jonas kervin Hansen <jonas.kerwin.hansen@gmail.com>
2017-08-18 20:54:40 +0200

    Replies seeder

---
+++ database/seeds/RepliesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Reply;
+use Illuminate\Database\Seeder;
+
+class RepliesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Reply::class)->times(50)->create();
+    }
+}
