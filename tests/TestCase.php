d847077b87c93a9590482c16bfd703a741138a9c
Jonas kervin Hansen <jonas.kerwin.hansen@gmail.com>
2017-08-19 11:52:35 +0200

    Better test signIn.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,19 +22,28 @@ abstract class TestCase extends BaseTestCase
         $this->seed(\PermissionsTableSeeder::class);
     }
 
+    protected function signInAsModerator($user = null)
+    {
+        return $this->signInWithRole('Moderator', $user);
+    }
+
+    protected function signInAsAdmin($user = null)
+    {
+        return $this->signInWithRole('Admin', $user);
+    }
+
+    protected function signInWithRole($role, $user = null)
+    {
+        return tap($this->signIn($user), function () use($role){
+            auth()->user()->assignRole($role);
+        });
+    }
+
     protected function signIn($user = null)
     {
         Passport::actingAs($user ? $user : factory(User::class)->create());
 
         return $this;
-    }
-
-    protected function signInAsAdmin($user = null)
-    {
-        $user = $user ? $user : factory(User::class)->create();
-        $user->assignRole('Admin');
-
-        return $this->signIn($user);
     }
 
     protected function disableExceptionHandling()
