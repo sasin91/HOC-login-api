10c50f40777a09cbe3ec6ede2a5fdf235b89ed7d
Jonas kervin Hansen <jonas.kerwin.hansen@gmail.com>
2017-09-07 22:11:26 +0200

    Unique user.name

---
+++ database/migrations/2017_09_07_201007_add_unique_to_users_name.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddUniqueToUsersName extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function (Blueprint $table) {
+			$table->unique('name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users_name', function (Blueprint $table) {
+			//
+		});
+	}
+}
